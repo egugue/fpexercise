2fd2132de43bab6c7f405e32d7e77fe23bda25c9
Haruyoshi Toyama <duane0728@gmail.com>
2017-03-15 23:31:08 +0900

    Add ex1

--- src/test/java/com/egugue/fpexercise/e1/CafeTest.kt
+++ src/test/java/com/egugue/fpexercise/e1/CafeTest.kt
@@ -1,8 +1,29 @@
 package com.egugue.fpexercise.e1
 
-import org.junit.Assert.*
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
 
-/**
- * Created by htoyama on 2017/03/15.
- */
-class CafeTest
+class CafeTest {
+
+  @Test fun foo() {
+    val cc1 = CreditCard()
+    val charges1 = listOf(
+        Charge(cc1, 10.0),
+        Charge(cc1, 10.0),
+        Charge(cc1, 10.0))
+
+    val cc2 = CreditCard()
+    val charges2 = listOf(
+        Charge(cc2, 10.0),
+        Charge(cc2, 20.0),
+        Charge(cc2, 30.0))
+
+    val merged = charges1.plus(charges2)
+    val actual = Cafe().coalesce(merged)
+
+    assertThat(actual).isEqualTo(listOf(
+        Charge(cc1, 30.0),
+        Charge(cc2, 60.0)
+    ))
+  }
+}
